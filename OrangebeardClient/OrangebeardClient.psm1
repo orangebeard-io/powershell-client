a2bb2eb2786bf85b490d0638ee4c229113228a1b
Tcnh <tom.heintzberger@gmail.com>
2020-09-01 12:05:30 +0200

    First version

---
+++ OrangebeardClient/OrangebeardClient.psm1
@@ -0,0 +1,223 @@
+enum LogLevel {
+    error
+    warn
+    info
+    debug
+    trace
+    fatal
+    unknown
+}
+
+enum Status {
+    PASSED
+    FAILED
+    STOPPED
+    SKIPPED
+    RESETED
+    CANCELLED
+}
+
+enum TestItemType {
+    SUITE
+    STORY
+    TEST 
+    SCENARIO 
+    STEP 
+    BEFORE_CLASS 
+    BEFORE_GROUPS
+    BEFORE_METHOD 
+    BEFORE_SUITE 
+    BEFORE_TEST 
+    AFTER_CLASS 
+    AFTER_GROUPS
+    AFTER_METHOD 
+    AFTER_SUITE
+    AFTER_TEST
+}
+
+class Attribute {
+    [string]$key
+    [string]$value
+}
+
+class AttachmentFile {
+    [string]$name
+    
+    AttachmentFile([string]$fileName) {
+        $this.name = [System.IO.Path]::GetFileName($fileName)
+    }
+}
+
+class Log {
+    [string]$launchUuid
+    [string]$itemUuid
+    [string]$level
+    [string]$message
+    [AttachmentFile]$file
+    [string]$time
+
+    Log([string]$testRunUUID, [string]$itemUuid, [string]$loglevel, [string]$message, [string]$file) {
+            $this.launchUuid = $testRunUUID
+            $this.itemUuid = $itemUuid
+            $this.level = $loglevel
+            $this.message = $message
+            $this.time = [int64](([datetime]::UtcNow)-(get-date "1/1/1970")).TotalMilliseconds
+            $this.file = [AttachmentFile]::new($file)
+        }
+
+    Log([string]$testRunUUID, [string]$itemUUID, [string]$loglevel, [string]$message) {
+            $this.launchUuid = $testRunUUID
+            $this.itemUuid = $itemUuid
+            $this.level = $loglevel
+            $this.message = $message
+            $this.time = [int64](([datetime]::UtcNow)-(get-date "1/1/1970")).TotalMilliseconds
+        }
+}
+
+class StartTestRun {
+    [string]$mode = 'DEFAULT'
+    [bool]$rerun = $false
+    [string]$rerunOf = $null
+
+    [string]$name
+    [string]$description
+    [string]$startTime
+
+    StartTestRun([string]$description, [string]$name) {
+        $this.description = $description
+        $this.name = $name
+        $this.startTime = [int64](([datetime]::UtcNow)-(get-date "1/1/1970")).TotalMilliseconds
+    }
+}
+
+class StartTestItem {
+    [string]$launchUuid
+    [string]$type
+    
+    [string]$name
+    [string]$description
+    [string]$startTime
+
+    StartTestItem([string]$testRunUUID, [string]$type, [string]$description, [string]$name) {
+        $this.launchUuid = $testRunUUID
+        $this.type = $type
+        $this.description = $description
+        $this.name = $name
+        $this.startTime = [int64](([datetime]::UtcNow)-(get-date "1/1/1970")).TotalMilliseconds
+    }
+}
+
+class FinishTestRun {
+    [string]$endTime
+    [string]$status
+
+    FinishTestRun([string]$status) {
+        $this.status = $status
+        $this.endTime = [int64](([datetime]::UtcNow)-(get-date "1/1/1970")).TotalMilliseconds
+    }
+}
+
+class FinishTestItem {
+    [string]$launchUuid
+    [string]$endTime
+    [string]$status
+
+    FinishTestItem([string]$testrunUUID, [string]$status) {
+        $this.launchUuid = $testrunUUID
+        $this.endTime = [int64](([datetime]::UtcNow)-(get-date "1/1/1970")).TotalMilliseconds
+        $this.status = $status
+    }
+}
+
+
+#####      <<----------------- CLIENT LOGIC ----------------->>      #####
+##########################################################################
+
+class OrangebeardClient {
+    [string]$orangebeard_endpoint = $env:orangebeard_endpoint;
+    [string]$orangebeard_project = $env:orangebeard_project;
+    [string]$orangebeard_token = $env:orangebeard_token;
+    [string]$orangebeard_testRun = $env:orangebeard_testrun;
+    
+    hidden [String]$apiVersion = "v1"
+
+    hidden [string]$testrunUUID; 
+    hidden [string]$lastRootItem;
+    hidden [string]$lastItem;
+
+    hidden [bool]$rerun;
+    hidden [string]$rerunOf;
+
+    [string] $projectApiUrl = $this.orangebeard_endpoint + "/api/" + $this.apiVersion + "/" + $this.orangebeard_project
+
+    reportStartTestRun([string]$description, [string]$name){
+        [string] $url = $this.projectApiUrl + "/launch"
+        [StartTestRun] $startTestRun = [StartTestRun]::new([string]$description, [string]$name)
+        
+        $body = $startTestRun | ConvertTo-Json
+        $response = Invoke-RestMethod -Headers @{Authorization = "bearer " + $this.orangebeard_token} -Method Post -Uri $url -Body $body -ContentType "application/json"
+        $this.testrunUUID = $response.id
+    }
+
+    reportFinishTestRun([Status]$status) {
+        [String] $url = $this.projectApiUrl +  "/launch/" + $this.testrunUUID + "/finish"
+        [FinishTestRun] $finishTestRun = [FinishTestRun]::new($status)
+        $body = $finishTestRun | ConvertTo-Json
+        Invoke-RestMethod -Headers @{Authorization = "bearer " + $this.orangebeard_token} -Method put -Uri $url -Body $body -ContentType "application/json"
+    }
+
+    reportStartTestCase([string]$name, [string]$description, [TestItemType]$type) {
+        [string] $url = $this.projectApiUrl + "/item"
+        [StartTestItem] $startItem = [StartTestItem]::new($this.testrunUUID, $type.ToString(), $description, $name)
+        $body = $startItem | ConvertTo-Json
+        $response = Invoke-RestMethod -Headers @{Authorization = "bearer " + $this.orangebeard_token} -Method Post -Uri $url -Body $body -ContentType "application/json"
+        $this.lastItem = $response.id
+    }
+
+    reportFinishTestCase([Status]$status) {
+        [string] $url = $this.projectApiUrl + "/item/" + $this.lastItem 
+        [FinishTestItem] $finishItem = [FinishtestItem]::new($this.testrunUUID, $status)
+        $body = $finishItem | ConvertTo-Json
+        Invoke-RestMethod -Headers @{Authorization = "bearer " + $this.orangebeard_token} -Method Put -Uri $url -Body $body -ContentType "application/json"
+    }
+
+    reportLog([string]$message, [LogLevel]$level) {
+        [string] $url = $this.projectApiUrl + "/log"
+        [Log] $logItem = [Log]::new($this.testrunUUID, $this.lastItem, $level, $message)
+        $body = $logItem | ConvertTo-Json
+        Invoke-RestMethod -Headers @{Authorization = "bearer " + $this.orangebeard_token} -Method Post -Uri $url -Body $body -ContentType "application/json"
+    }
+
+    reportAttachment([string]$message, [LogLevel]$level, [string]$file) {
+        [string] $url = $this.projectApiUrl + "/log"
+        [Log] $logItemWithAttachment = [Log]::new($this.testrunUUID, $this.lastItem, $level, $message, $file)
+        
+        $fileName = [System.IO.Path]::GetFileName($file)
+        $fileBytes = [System.IO.File]::ReadAllBytes($file);
+        $fileEnc = [System.Text.Encoding]::GetEncoding('ISO-8859-1').GetString($fileBytes);
+        $fileMimeType = [System.Web.MimeMapping]::GetMimeMapping($file.FullName)
+        $boundary = [System.Guid]::NewGuid().ToString(); 
+        
+        $jsonBody = $logItemWithAttachment | ConvertTo-Json
+        $LF = "`r`n";
+
+        $body = (     
+            "--$boundary",
+            "Content-Disposition: form-data; name=`"json_request_part`"",
+            "Content-Type: application/json$LF",
+            "[$jsonBody]",    
+            "--$boundary",
+            "Content-Disposition: form-data; name=`"file`"; filename=`"$filename`"",
+            "Content-Type: $fileMimeType$LF",
+            $fileEnc,
+            "--$boundary--$LF" 
+        ) -join $LF
+        
+        Invoke-RestMethod -Headers @{Authorization = "bearer " + $this.orangebeard_token} -Method Post -Uri $url -ContentType "multipart/form-data; boundary=`"$boundary`"" -Body $body
+    }
+
+
+
+}
+
+
---
+++ README.md
@@ -0,0 +1,106 @@
+<h1 align="center">
+  <a href="https://github.com/orangebeard-io/powershell-client">
+    <img src="https://raw.githubusercontent.com/orangebeard-io/powershell-client/master/.github/logo.png" alt="Orangebeard.io Powershell Client" height="200">
+  </a>
+  <br>Orangebeard.io Powershell Client<br>
+</h1>
+
+<h4 align="center">A test output listener for Praegus Open Source Toolchain FitNesse tests.</h4>
+
+<!--<p align="center">
+  <a href="https://repo.maven.apache.org/maven2/io/orangebeard/fitnesse-toolchain-listener/">
+    <img src="https://img.shields.io/maven-central/v/io.orangebeard/fitnesse-toolchain-listener.svg?maxAge=3600&style=flat-square"
+      alt="MVN Version" />
+  </a>
+  <a href="https://github.com/orangebeard-io/fitnesse-toolchain-listener/actions">
+    <img src="https://img.shields.io/github/workflow/status/orangebeard-io/fitnesse-toolchain-listener/release?style=flat-square"
+      alt="Build Status" />
+  </a>
+  <a href="https://github.com/orangebeard-io/fitnesse-toolchain-listener/blob/master/LICENSE.txt">
+    <img src="https://img.shields.io/github/license/orangebeard-io/fitnesse-toolchain-listener?style=flat-square"
+      alt="License" />
+  </a>
+</p>-->
+
+<div align="center">
+  <h4>
+    <a href="https://orangebeard.io">Orangebeard</a> |
+    <a href="#installation">Installation</a> |
+    <a href="#configuration">Configuration</a>
+  </h4>
+</div>
+
+## Installation
+Copy the OrangebeardClient folder to your PowerShell Modules directory 
+
+## Configuration
+Start you script with: `using module OrangebeardClient`
+
+Set the following environment variables:
+```
+$env:orangebeard_endpoint = "https://my.orangebeard.app";
+$env:orangebeard_project = "your-project";
+$env:orangebeard_token = "your-access-token";
+$env:orangebeard_testrun = "your-testrun-name";
+```
+
+Start the client using: `$orangebeard = [OrangebeardClient]::new()`
+
+The orangebeard client currently supports the following methods:
+
+### reportStartTestRun ("Test run name", "Description")
+Starts the testrun in Orangebeard.
+
+### reportFinishTestRun (Status)
+Finishes the test run and sets the desired [Status]
+
+### reportStartTestCase ("Test Case Name", "Description", TestItemType)
+report the start of a test case.
+
+### reportFinishTestCase (Status)
+Finishes the test case and sets the desired [Status]
+
+### reportLog ("Message", LogLevel)
+Logs a message to the current test report.
+
+### reportAttachment("Message", LogLevel, "file path") 
+Logs an attachment file to the current test report.
+
+## Enumerations
+
+### LogLevel
+ * error
+ * warn
+ * info
+ * debug
+ * trace
+ * fatal
+ * unknown
+
+### Status
+ * PASSED
+ * FAILED
+ * STOPPED
+ * SKIPPED
+ * RESETED
+ * CANCELLED
+
+### TestItemType
+ * SUITE
+ * STORY
+ * TEST 
+ * SCENARIO 
+ * STEP 
+ * BEFORE_CLASS 
+ * BEFORE_GROUPS
+ * BEFORE_METHOD 
+ * BEFORE_SUITE 
+ * BEFORE_TEST 
+ * AFTER_CLASS 
+ * AFTER_GROUPS
+ * AFTER_METHOD 
+ * AFTER_SUITE
+ * AFTER_TEST
+
+## Limitations
+ - Currently, only flat suites are supported (one suite, containing testcases). No further nesting logic is implemented
